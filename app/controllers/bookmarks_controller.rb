148e46368336130a78eb4ee0bf891a98d5f55c14
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-08-16 12:33:18 +0200

    Refactored edit list, add bookmark and create list to modals

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,47 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = List.find(params[:list_id])
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      # redirect_to root_path
+      flash[:notice] = 'Post successfully created'
+    end
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.update(bookmark_params)
+    redirect_to bookmark_path(@bookmark)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
